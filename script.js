487bee78402a5f185ebd0c8c75bc578e378b4c32
Fabien Brun <fabienbrun@MacBook-Pro-de-Fabien.local>
2020-03-11 16:07:36 +0100

    add btn with clicked action

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
+    var specialElementHandler = {
+        "#editor":function(element,renderer){
+            return true;
+        }
+    };
 
+    $("#cmd").click(function(){
+        console.log("Clicked !!")
+    })
 
 })
